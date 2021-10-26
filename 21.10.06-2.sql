45be07cc2272b8c8a704e4f8bf40a9a2f6f833dd
LIVINGROOM-DESK\Home <ebyul96428@naver.com>
2021-10-26 18:00:58 +0900

    21.10.26업로드

--- 21.10.06-2.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-	COUNT(*)
-FROM
-	phonenumber_list_mst
-WHERE
-	phone_number = '01011112222'
-AND phone_user_name = '김 준 일';
--- 21.10.07.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	COUNT(*)
-FROM
-	phonenumber_list_mst
-WHERE
-	phone_number = '01011112222'
-AND
-	phone_user_name = '김준일';
